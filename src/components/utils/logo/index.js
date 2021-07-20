be77a34268f89a50b9edb165cdb6c4c1bdf12d11
Mathias Payen <mathias.payen@gmail.com>
2021-07-20 11:58:29 +0200

    logo link

--- src/components/utils/logo/index.js
+++ src/components/utils/logo/index.js
@@ -3,13 +3,14 @@ import fd_logo from '../../../assets/design/logo/fd_logo.svg';
 import aftw_logo from '../../../assets/design/logo/logo_aftrwrk.svg';
 import style from './Logo.module.scss';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
  
 
 const Logo = ({
     name,
 }) => {
     const display = name === 'fulldub' ? fd_logo : name === 'afterwork' ? aftw_logo : fd_logo; 
-    return <img alt="logo" className={style.logo} src={display} />;
+    return  <NavLink to="/home" className={style.logo}><img alt="logo"  src={display} /></NavLink>;
 }
 
 Logo.propTypes = {
--- src/conf/api.fulldub.js
+++ src/conf/api.fulldub.js
@@ -1,16 +1,11 @@
 import * as axios from 'axios';
 
 const apiFulldub = axios.create({
-    // baseURL: 'https://boodu.fr',
-    // baseURL: `http://localhost:3030`
-    baseURL: 'https://fulldub.fr'
+    baseURL: `http://localhost:3030`
+    // baseURL: 'https://fulldub.fr'
 })
 
 apiFulldub.interceptors.request.use( req => {
-    // req.headers['Authorization'] =  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YTQ5OTk4YWEzNGQ4YTA0MjhlNGY0Njc5NTc0MTM0YyIsInN1YiI6IjVmNDc5MmJlODEzY2I2MDAzNjhjYTM5MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q9erEZ0XAq2dn1qFPI7Oq5DXLoKlZ1lmSXK8qDWCkNE',
-    // req.headers = {
-    //     'auth-token': `${localStorage.getItem('jwtToken')}`
-    //     };
     return req;
 })
 
