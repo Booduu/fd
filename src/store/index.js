74b10dae28a063a73d6cde631ea6e33da308ac3d
Mathias Payen <mathias.payen@gmail.com>
2021-08-31 10:55:16 +0200

    display reduxdevtool only on development

--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,43 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import myMiddlewares from './middlewares/middlewares';
 
+// const composer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+
+
+// let middlewares = [
+//     thunkMiddleware,
+//     myMiddlewares,
+// ];
+
+
+// const reducers = combineReducers(myReducers);
+
+// const store = createStore(reducers, composer(applyMiddleware(...middlewares)));
+
+// export default store;
+
+
 const composer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+console.log('process.env.NODE_ENV', process.env.NODE_ENV);
 
 let middlewares = [
     thunkMiddleware,
     myMiddlewares,
 ];
 
+const devTools =
+  process.env.NODE_ENV === "production"
+    ? applyMiddleware(...middlewares)
+    : composer(applyMiddleware(...middlewares));
+
 const reducers = combineReducers(myReducers);
 
-const store = createStore(reducers, composer(applyMiddleware(...middlewares)));
+const store = createStore(reducers, devTools);
 
 export default store;
+
+
+
+
