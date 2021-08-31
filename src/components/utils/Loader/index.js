49f9972bb7841441a6e64dcfdc6c39dd8a6a5509
Mathias Payen <mathias.payen@gmail.com>
2021-08-31 10:18:45 +0200

    anim loader css

--- src/components/utils/Loader/index.js
+++ src/components/utils/Loader/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from './Loader.module.scss';
 
-const Loader = () => {
+const Loader = React.memo(() => {
+    console.log('loader');
     return (  
         <div className={styles.container}>
             <h4 className={styles.title}>Loading...</h4>
             <div className={styles.loading}>
-                <div className={styles.anim}></div>
+                <div className={styles.barloaderContainer}></div>
             </div>
         </div>
     );
-}
+})
  
 export default Loader;
