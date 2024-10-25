d341ad2df61c1e281fb827db9e185413c81e4d45
mathias <mathias.payen@gmail.com>
2024-10-25 20:43:17 +0200

    pro url

--- src/conf/api.fulldub.js
+++ src/conf/api.fulldub.js
@@ -1,6 +1,8 @@
 import * as axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === "production" ? 'https://fulldub.fr' : 'http://localhost:3030'
+// const baseURL = process.env.NODE_ENV === "production" ? 'https://fulldub.fr' : 'http://localhost:3030'
+
+const baseURL =  'https://fulldub.fr'
 
 const apiFulldub = axios.create({
     baseURL,
